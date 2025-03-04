f425531d833b59e1daf710d2a8b6d6220e6c5aae
mco08gx7i <adm98xc0o@metatrendfuture.com>
2025-03-04 23:44:06 +0800

    Initial

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ const fontSans = FontSans({
 });
 
 export const metadata: Metadata = {
-  title: "CarePulse",
+  title: "Orin",
   description:
-    "A healthcare patient management System designed to streamline patient registration, appointment scheduling, and medical records management for healthcare providers.",
+    "",
   icons: {
     icon: "/assets/icons/logo-icon.svg",
   },
